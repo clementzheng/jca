f652ba882c87884fce3fec7c0fae615d19ab2137
clementzheng <clement.zheng@gmail.com>
2016-09-01 21:39:56 -0400

    project created

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*detecting if device is mobile*/
+
+var mobile = false;
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    mobile = true;
+}
+
+if (mobile || $(window).innerWidth()<900) {
+	$(".nav_bar").css("display", "none");
+	$(".nav_bar_mobile").css("display", "block");
+}
+
+$(window).on('resize', function() {
+	if ($(window).innerWidth()<900 || mobile) {
+		$(".nav_bar").css("display", "none");
+		$(".nav_bar_mobile").css("display", "block");
+	} else {
+		$(".nav_bar").css("display", "block");
+		$(".nav_bar_mobile").css("display", "none");
+	}	
+});
+
+$(".nav_menu_icon").on('click', function() {
+	$(this).parent().find(".nav_content_mobile").toggleClass("active");
+	$("#open_nav").toggleClass("active");
+	$("#close_nav").toggleClass("active");
+});
+
+if ($("#emergency_banner").css("display")=="block") {
+	$(".nav_bar").css("top", "60px");
+	$(".nav_menu_icon").css("top", "60px");
+}
