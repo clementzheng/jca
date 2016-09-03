b07bf834d4973f205ed465185982290dc2ad1752
clementzheng <clement.zheng@gmail.com>
2016-09-03 16:28:17 -0400

    minor fixes

--- js/script.js
+++ js/script.js
@@ -17,9 +17,9 @@ if (mobile || $(window).innerWidth()<900) {
 if (mobile) {
 	$(".container").css("width", "100%");
 	$(".container").css("padding", "0 20px");
-	$(".nav_content_mobile .nav_item .nav_title").css("font-size", "6vw");
+	$(".nav_content_mobile .nav_item .nav_title").css("font-size", "5vw");
 	$(".nav_content_mobile .nav_item .nav_title").css("line-height", "6vw");
-	$(".nav_content_mobile .nav_item .nav_link").css("font-size", "6vw");
+	$(".nav_content_mobile .nav_item .nav_link").css("font-size", "5vw");
 	$(".nav_content_mobile .nav_item .nav_link").css("line-height", "6vw");
 }
 
